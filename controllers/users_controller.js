62c90509d0351b392acc1f250ac8e46606d71d73
Preetam1211 <104054103+Preetam1211@users.noreply.github.com>
2023-03-31 22:56:52 +0530

    Sixth commit : create user schema and sign-in sign-up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,19 @@ module.exports.profile = function(req,res){
         title : "user profile"
     });
 }
+
+module.exports.sign_up = function(req,res){
+    res.render('sign_up',{
+        title : 'Sign Up'
+    });
+}
+
+module.exports.sign_in = function(req,res){
+    res.render('sign_in',{
+        title : 'Sign In'
+    });
+}
+
+module.exports.createuser = function(req,res){
+    // todo later
+}
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,10 @@ const userscontroller = require('../controllers/users_controller');
 
 router.get('/profile',userscontroller.profile);
 
+router.get('/sign-up',userscontroller.sign_up);
+
+router.get('/sign-in',userscontroller.sign_in);
+
+router.post('/create-user',userscontroller.createuser);
+
 module.exports = router;
