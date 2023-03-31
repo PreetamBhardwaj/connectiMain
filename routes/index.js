dee5440448b212433d1c23e0a113e3f2027df405
Preetam1211 <104054103+Preetam1211@users.noreply.github.com>
2023-03-31 18:00:34 +0530

    third commit : add route and controller for home and users(/users/profile)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const homecontroller = require('../controllers/home_controller');
 
 console.log("router loaded");
+
+router.get('/',homecontroller.home);
+
+router.use('/users',require('./users'));
 
 module.exports = router;
